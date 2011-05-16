4e56d4b11284f9c62f8484c64081662af44138a1
Ricky Elrod <ricky@elrod.me>
2011-05-16 14:36:44 -0400

    Sorta-blank Rails project. Includes initial Quote scaffold.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acmqdb::Application.config.secret_token = 'cface6c86b5c7b24655599ebae6f085bf6cc616aafad9b5c8283416660a17c87dace56fdc9e39b468c14598da416e685b1a49c380d37707ab9d81143ae2375e1'
