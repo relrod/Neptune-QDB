2bcbbb0ee5eae3d75fd8bf9eb83c04d9f5fddd27
Ricky Elrod <ricky@elrod.me>
2011-05-16 19:48:14 -0400

    Well this is pointless

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ActiveRecord::Base
   belongs_to :quote
-  validates_uniqueness_of :ip_address, :scope => :quote_id, :message => "has already rated this quote."
+  #validates_uniqueness_of :ip_address, :scope => :quote_id, :message => "has already rated this quote."
   validates_presence_of :quote_id, :ip_address, :direction
   validates_associated :quote
 end
